07cd34e3772413f2f46dd90c17573140cc39078f
Thomas Dhaussy <94963203+DHS5@users.noreply.github.com>
2023-12-26 15:42:31 +0100

    Update basic.hlsl

--- basic.hlsl
+++ basic.hlsl
@@ -12,11 +12,13 @@ cbuffer cbPerPass : register(b1)
 struct VertexIn
 {
     float3 Pos : POSITION;
+    float4 Color : COLOR;
 };
 
 struct VertexOut
 {
     float4 Pos : SV_POSITION;
+    float4 Color : COLOR;
 };
 
 VertexOut VS(VertexIn vin)
@@ -25,6 +27,7 @@ VertexOut VS(VertexIn vin)
 	
 	// Transform to homogeneous clip space.
     vout.Pos = mul(float4(vin.Pos, 1.0f), gWorldViewProj);
+    vout.Color = vin.Color;
     
     return vout;
 }
